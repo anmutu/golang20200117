be01ba8c75fb0b769479ffca484e2f653619e6db
anmutu <anmutu@hotmail.com>
2020-01-29 15:25:03 +0800

    完善ParseResult的结构体。

--- crawler/engine/types.go
+++ crawler/engine/types.go
@@ -13,7 +13,14 @@ type Request struct {
 //返回的结构体，其中"interface{}"表示任何表示任何类型，有点类似c#里的泛型T
 type ParseResult struct {
 	Requests []Request
-	Items    []interface{}
+	Items    []Item
+}
+
+type Item struct {
+	Url     string
+	Type    string
+	Id      string
+	PayLoad interface{}
 }
 
 func NilParser([]byte) ParseResult {
