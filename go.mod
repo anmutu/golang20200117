b6c750ad14322eff01230cbb47a1b6d1e4d861a3
anmutu <anmutu@hotmail.com>
2020-02-06 22:27:32 +0800

    gorm

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	github.com/garyburd/redigo v1.6.0 // indirect
 	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/olivere/elastic/v7 v7.0.10
 	go.uber.org/zap v1.13.0 // indirect
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
---
+++ gorm/main.go
@@ -0,0 +1,39 @@
+/*
+  author='du'
+  date='2020/2/6 21:15'
+*/
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+func main() {
+	db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golangtestdb?charset=utf8")
+	db.SingularTable(true)
+	defer db.Close()
+	if err != nil {
+		log.Printf("连接db出错，error是:%s", err)
+	}
+
+	db.AutoMigrate(&User{})
+
+	//创建
+	db.Create(&User{Name: "du", Age: 18})
+
+	//读取
+	//var user User
+	//db.First(&user,1)
+
+	//删除
+	db.Delete(&user, 1)
+
+}
+
+type User struct {
+	gorm.Model
+	Name string
+	Age  int
+}
