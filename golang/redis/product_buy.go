c4df11a9d78cb9104ae613f1a25e46e5e884db08
anmutu <anmutu@hotmail.com>
2020-02-06 16:37:28 +0800

    向redis里push和pop数据的功能。

--- golang/redis/product_buy.go
+++ golang/redis/product_buy.go
@@ -4,6 +4,26 @@
 */
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+)
 
+const (
+	REDISKEY = "hbdw:fruit:apple:n500:1"
+)
+
+func main() {
+	conn, err := redis.Dial("tcp", "129.211.78.6:6379")
+	if err != nil {
+		fmt.Println("连接redis失败，失败的error是：", err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Do("lpush", REDISKEY, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	if err != nil {
+		fmt.Println("向redis里push数据失败，失败原因是：", err)
+		return
+	}
+	fmt.Println("成功向redis里push了数据")
 }
--- golang/redis/product_sale.go
+++ golang/redis/product_sale.go
@@ -4,6 +4,22 @@
 */
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+)
 
+func main() {
+	conn, err := redis.Dial("tcp", "129.211.78.6:6379")
+	if err != nil {
+		fmt.Println("连接redis失败，失败原因是:", err)
+	}
+	defer conn.Close()
+
+	item, err := redis.String(conn.Do("lpop", "hbdw:fruit:apple:n500:1"))
+	if err != nil {
+		fmt.Println("从redis中pop数据失败")
+		return
+	}
+	fmt.Println("成功从redis中pop出数据，数值是:", item)
 }
